e89cad32b71021cbc9a20f53e372ea5aef6c8e94
Ravi Raj <meraviraj05@gmail.com>
2024-07-23 22:35:40 +0530

    Student details and info and default Constructor

--- Program.cs
+++ Program.cs
@@ -14,16 +14,25 @@ namespace OOPS_Concept
         {
             public string Name{ get; set; }
             public int Age { get; set; }
-            public int Id { get; set; }
+            public string Id { get; set; }
+            public double MonthlyJ { get; set; }
+            public double MonthlyF { get; set; }
+            public double MonthlyM { get; set; }
 
 
-            public void StudentIndfo(int age, int id, string name)
+            public void StudentIndfo(int age, string id, string name,double monthlyj, double monthlyf, double monthlym)
             {
                 Name = name.ToLower();
                 Age = age;
                 Id = id;
+                MonthlyJ = monthlyj;
+                MonthlyF = monthlyf;
+                MonthlyM = monthlym;
 
                 Console.WriteLine("Student Info:" + "\nName:" + name + "\nAge: " + age + "\nId: " + id);
+
+                double sumM= monthlyf+monthlyj+monthlym;
+                Console.WriteLine("Total Monthly Till March: " + sumM);
 
             }
             public Student()
@@ -40,19 +49,25 @@ namespace OOPS_Concept
         {
            
 
-            Console.Write("Enter Student Info: ");
+            Console.WriteLine("Enter Student Info: ");
             Console.Write("Name: ");
             string name=Console.ReadLine();
             Console.Write("Age: ");
             int age =Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Id: ");
-            int id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Id: ");
+            string id = (Console.ReadLine());
+            Console.Write("January Monthly: ");
+            double jMonthly= double.Parse(Console.ReadLine());
+            Console.Write("February Monthly: ");
+            double fMonthly =double.Parse(Console.ReadLine());
+            Console.Write("March Monthly: ");
+            double mMonthly = double.Parse((Console.ReadLine()));
 
 
 
             Student student = new Student();
 
-            student.StudentIndfo(age, id, name);
+            student.StudentIndfo(age, id, name,jMonthly,fMonthly,mMonthly);
 
             Console.ReadLine();
         }
