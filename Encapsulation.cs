95f6e0954f365063ee63bb02048ae0132581aef6
Ravi Raj <meraviraj05@gmail.com>
2024-07-24 17:22:29 +0530

    Encapsulation

---
+++ Encapsulation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concept
+{
+    //Property and function are in one single class is called Encapsulation:
+
+    public class Shape
+    {
+        
+        double width;
+        double height;
+       public void CalculateArea(double width, double hight)
+        {
+            this.width = width;
+            this.height = hight;
+            double Area;
+            Area=hight*width;
+
+        }
+    }
+    internal class Encapsulation
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Shape();
+            double area = shape.Area(12, 15.5);
+            Console.WriteLine(area);
+            Console.ReadLine();
+
+        }
+    }
+}
